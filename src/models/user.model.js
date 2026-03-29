9e02e4a591b4c3be2bcda60ae0297cfe1d0a21ad
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-30 01:39:46 +0600

    feat: create User mongoose model with authentication and role fields

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        // trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "artist"],
+        default: "user",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const userModel = mongoose.model("User", userSchema);
+
+
+
+module.exports = userModel;
