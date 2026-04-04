8a23d5e619c9b9080c958634ff4485f45f761029
ShuvrotoKumar <sdks6409@gmail.com>
2026-04-05 02:55:12 +0600

    fix: remove extraneous whitespace in logoutUser cookie clearing method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -59,7 +59,7 @@ async function loginUser(req, res) {
 }
 
 async function logoutUser(req, res) {
-    res.    clearCookie("token");
+    res.clearCookie("token");
     return res.status(200).json({ message: "User logged out successfully" });
 }
 module.exports = { registerUser, loginUser, logoutUser };
