e60309d3c3b53bbc29d9cfcf1705c7e3a8adbb82
ShuvrotoKumar <sdks6409@gmail.com>
2026-04-05 02:27:53 +0600

    feat: add getAllAlbums controller to retrieve and populate album data

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -77,4 +77,15 @@ async function getAllMusic(req, res) {
         return res.status(500).json({ message: "Internal server error", error: error.message });
     }
 }
-module.exports = { createMusic, createAlbum, getAllMusic };
+
+async function getAllAlbums(req, res) {
+    try {
+        const albums = await albumModel.find().populate("artist", "username email");
+        return res.status(200).json({ message: "All albums fetched successfully", albums: albums });
+    } catch (error) {
+        console.error("Error getting all albums:", error);
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+module.exports = { createMusic, createAlbum, getAllMusic, getAllAlbums };
+    
